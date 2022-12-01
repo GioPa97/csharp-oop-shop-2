36e191537fa47c9991c6f067ea3eec493e9c6160
GioPa97 <giovannipagliara97@.com>
2022-12-01 15:11:18 +0100

    Aggiungere i file di progetto.

---
+++ CSharpShop 2/Elettrodomestico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop_2
+{
+    public class Elettrodomestico : prodotto
+    {
+        private string condizione;
+        public Elettrodomestico(string nome, string descrizione, string condizione, double prezzo, float iva) : base(nome, descrizione, prezzo, iva)
+        {
+            this.condizione = condizione;
+
+        }
+    }
+    
+}
---
+++ CSharpShop 2/Program.cs
@@ -0,0 +1,10 @@
+﻿using CSharpShop_2;
+
+//Creo un prodotto e lo stampo//___________________________________________________
+prodotto prodottoUno = new prodotto("pane", "alimentari", 3.99, 4);
+prodottoUno.stampaProdotto();
+
+//_________________________________________________________________________________
+//Creo un prodotto e lo stampo//___________________________________________________
+prodotto prodottoDue = new prodotto("sparapatate", "utensili", 119.99, 22);
+prodottoDue.stampaProdotto();
